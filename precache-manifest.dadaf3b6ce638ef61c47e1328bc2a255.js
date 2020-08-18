af6fbcd8c740df897646bf4358d6139c1505ae4b
Jesse Audije <audijej@gmail.com>
2020-08-17 22:42:14 -0700

    Updates

---
+++ precache-manifest.dadaf3b6ce638ef61c47e1328bc2a255.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bb2a66096a089176c38c81c3a3c660d",
+    "url": "/audijej.github.io/index.html"
+  },
+  {
+    "revision": "82ed0ec782cb0f813c66",
+    "url": "/audijej.github.io/static/css/main.bf0a1993.chunk.css"
+  },
+  {
+    "revision": "aa6bde1d7fc6bbf2300c",
+    "url": "/audijej.github.io/static/js/2.86e810ed.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/audijej.github.io/static/js/2.86e810ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82ed0ec782cb0f813c66",
+    "url": "/audijej.github.io/static/js/main.9863fc15.chunk.js"
+  },
+  {
+    "revision": "fadd01e001424df247dd",
+    "url": "/audijej.github.io/static/js/runtime-main.836a1d6a.js"
+  },
+  {
+    "revision": "7e21088578552074be84cfe22f783732",
+    "url": "/audijej.github.io/static/media/Cinementality.7e210885.png"
+  },
+  {
+    "revision": "7ecfdb2bde461534772d6beceb083011",
+    "url": "/audijej.github.io/static/media/PortfolioPic2.7ecfdb2b.jpg"
+  },
+  {
+    "revision": "8dd6811a469e0fa94eb8e7b9b782a833",
+    "url": "/audijej.github.io/static/media/PortfolioVideoBackground3.8dd6811a.mp4"
+  },
+  {
+    "revision": "61c088d1f6fe6f97db5089d9da020ec4",
+    "url": "/audijej.github.io/static/media/TeamGenerator.61c088d1.png"
+  },
+  {
+    "revision": "af7bd486d15fe1ea0e78d97fb201bc2e",
+    "url": "/audijej.github.io/static/media/codeQuiz.af7bd486.png"
+  },
+  {
+    "revision": "b38f8af3b0e5dee66ea1e6e2bed4c469",
+    "url": "/audijej.github.io/static/media/foodTruckTinder.b38f8af3.png"
+  },
+  {
+    "revision": "3a8390e4024aeee2093e07804fb0638b",
+    "url": "/audijej.github.io/static/media/weatherDashboard.3a8390e4.png"
+  },
+  {
+    "revision": "79e025149fea0ace6b6f4316d032b70d",
+    "url": "/audijej.github.io/static/media/workdayPlanner.79e02514.png"
+  }
+]);
